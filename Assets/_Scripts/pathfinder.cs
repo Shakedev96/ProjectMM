74ff9a30895823fe25eaa1a509d029f9730bcf73
4manfear <rickroad774@gmail.com>
2024-09-01 20:47:14 +0530

    nothing

---
+++ Assets/_Scripts/pathfinder.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class pathfinder : MonoBehaviour
+{
+    public GameObject destinationpoint;
+    public GameObject Player;
+
+    public TrailRenderer TR;
+
+    
+
+    private void Update()
+    {
+        
+    }
+}
